ba3628943629ad3fd8c98e3b4bbcc066b54d306c
Tony Li <394287122@qq.com>
2019-01-03 09:48:07 +0800

    fs

--- 9.fs/2.rmdir.js
+++ 9.fs/2.rmdir.js
@@ -12,18 +12,71 @@ let path = require('path')
 // fs.rmdirSync(path.resolve(__dirname, 'a/b'))
 
 // 删除的过程就是先删除儿子，再删除自己
-let dirList = fs.readdirSync(path.resolve(__dirname, 'a'))
-dirList = dirList.map(item => path.resolve(__dirname, 'a', item))
-dirList.forEach(dir => {
-    let statObj = fs.statSync(dir)
-    if (statObj.isFile()) {
-        fs.unlinkSync(dir)
-    } else {
-        fs.rmdirSync(dir)
-    }
-})
-fs.rmdirSync(path.resolve(__dirname, 'a'))
+// let dirList = fs.readdirSync(path.resolve(__dirname, 'a'))
+// dirList = dirList.map(item => path.resolve(__dirname, 'a', item))
+// dirList.forEach(dir => {
+//     let statObj = fs.statSync(dir)
+//     if (statObj.isFile()) {
+//         fs.unlinkSync(dir)
+//     } else {
+//         fs.rmdirSync(dir)
+//     }
+// })
+// fs.rmdirSync(path.resolve(__dirname, 'a'))
 
 // readdirSync、statSync（isFile）、unlinkSync、rmdirSync
 
 // 先序，中序，后序
+
+// 先序 深度 同步
+// function removeDirSync(p) {
+//     // 判断类型 fs.statSync
+//     let statObj = fs.statSync(p)
+//     if (statObj.isDirectory()) {
+//         // todo...
+//         let dirs = fs.readdirSync(p)
+//         console.log(dirs)
+//         // 拿到子元素，删除子元素
+//         dirs.forEach(dir => {
+//             // 循环儿子路径，拼接父亲路径，如果是文件夹，就要递归删除
+//             let currentPath = path.join(p, dir)
+//             console.log(currentPath)
+//             removeDirSync(currentPath)
+//         })
+//         // // 删除自己
+//         fs.rmdirSync(p)
+//     } else {
+//         // 如果是文件，直接删除
+//         fs.unlinkSync(p)
+//     }
+// }
+// removeDirSync(path.resolve(__dirname, 'a'))
+
+// 先序 深度 异步 串行
+function removeDir(p, cb) {
+    // 判断类型
+    fs.stat(p, function (err, statObj) {
+        if (err) {
+            return console.log(err)
+        }
+        if (statObj.isDirectory) {
+            fs.readdir(p, function (err, dirs) {
+                // 读取目录，如果没有儿子，就把自己删除
+                if (dirs.length === 0) fs.rmdir(p, cb)
+                dirs = dirs.map(dir => path.join(p, dir))
+                let index = 0
+                function next(i) {
+                    // 先删除 b 目录，后面放一个回调，当自己删除后，再调用回调继续删除
+                    removeDir(dirs[i], () => next(i + 1))
+                }
+                next(index)
+            })
+        } else {
+            // 如果是文件，直接删除
+            fs.unlink(p, cb)
+        }
+    })
+}
+removeDir(path.resolve(__dirname, 'a'), function () {
+    console.log('先序深度异步串行删除')
+})
