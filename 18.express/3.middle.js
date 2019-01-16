0e43c84d73fdacbc375a0dc07c73e8381dfd6b43
Tony Li <394287122@qq.com>
2019-01-16 18:24:22 +0800

    express中间件

--- 18.express/3.middle.js
+++ 18.express/3.middle.js
@@ -1,11 +1,43 @@
-let express = require('express')
+let express = require('./express')
+let path = require('path')
+
+// 监听函数
+let app = express()
+
+// 内置了中间件
+app.use(express.static(__dirname))
 
 // 中间件特点：
 // 1、决定是否向下执行
 // 2、扩展 req，res 属性
 // 3、常见的权限校验
 
-// 监听函数
-let app = require('express')
+// app.use(function (req, res, next) {
+//     res.send = function () {
+        
+//     }
+//     next()
+// })
 
+// 中间件只要开头匹配就可以匹配上
+app.use('/', function (req, res, next) {
+    console.log(1)
+    next('出错了')
+})
+app.use(function (req, res, next) {
+    console.log(2)
+    next()
+})
+// 错误处理中间件，一般在路由下方，参数是4个
+app.use(function (err, req, res, next) {
+    // console.log(err)
+    // res.send(404)
+    console.log(req.path)
+    console.log(req.query)
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
 app.listen(3000)
+
+// koa-static 判断是否为静态文件
+
+// bodyParser cookie-parser express-session 二级路由
--- 18.express/express/index.js
+++ 18.express/express/index.js
@@ -1,6 +1,8 @@
 let http = require('http')
 let methods = require('methods')
 let url = require('url')
+let fs = require('fs')
+let path = require('path')
 
 // 批量产生方法
 // console.log(methods)
@@ -10,33 +12,106 @@ function Application() {
         // 请求路径，请求方法
         let {pathname} = url.parse(req.url, true)
         let method = req.method.toLowerCase()
-        for (let i = 0; i < app.routes.length; i++) {
-            let layer = app.routes[i]
-            let {method: m, path, cb} = layer
-            // 如果有 params，这个路由是路径参数
-            if (layer.params) {
-                // 如果 /user/lyy/18 匹配 /\/user\/([^\/]*)\/([^\/]*)/
-                if (path.test(pathname)) {
-                    // 匹配到了，说明路径已经匹配到了，取出参数
-                    let [, ...args] = pathname.match(path)
-                    // 使用 reduce 赋值给 req.params
-                    req.params = layer.params.reduce((memo, key, index) => {
-                        memo[key] = args[index]
-                        return memo
-                    }, {})
-                    return cb(req, res)
+        let i = 0
+        function next(err) {
+            if (app.routes.length === i) {
+                return res.end(`Cannot ${method} ${pathname}`)
+            }
+            let layer = app.routes[i++]
+            let {
+                method: m,
+                path,
+                cb
+            } = layer
+
+            if (err) {
+                if (m === 'middleware') {
+                    if ((path === '/' || path === pathname || pathname.startsWith(path + '/')) && cb.length === 4) {
+                        return cb(err, req, res, next)
+                    } else {
+                        // 把错误继续带下去
+                        next(err)
+                    }
+                } else {
+                    next(err)
+                }
+            } else {
+                // 方法可能有两类，一类是中间件，另一类是普通的路由
+                if (m === 'middleware') {
+                    // if (err) {
+
+                    // } else {
+                    // 中间件特点会匹配路径，如果开头相同就可以匹配
+                    if (path === '/' || path === pathname || pathname.startsWith(path + '/')) {
+                        return cb(req, res, next)
+                    } else {
+                        next()
+                    }
+                    // }
+                } else {
+                    // 如果有 params，这个路由是路径参数
+                    if (layer.params) {
+                        // 如果 /user/lyy/18 匹配 /\/user\/([^\/]*)\/([^\/]*)/
+                        if (path.test(pathname)) {
+                            // 匹配到了，说明路径已经匹配到了，取出参数
+                            let [, ...args] = pathname.match(path)
+                            // 使用 reduce 赋值给 req.params
+                            req.params = layer.params.reduce((memo, key, index) => {
+                                memo[key] = args[index]
+                                return memo
+                            }, {})
+                            return cb(req, res)
+                        }
+                    }
+                    if ((method === m || m === 'all') && (path === pathname || path === '*')) {
+                        return cb(req, res)
+                    }
+                    next()
                 }
             }
-            if ((method === m || m === 'all') && (path === pathname || path === '*')) {
-                return cb(req, res)
-            }
         }
+        next()
+        // for (let i = 0; i < app.routes.length; i++) {
+            // let layer = app.routes[i]
+            // let {method: m, path, cb} = layer
+            // // 如果有 params，这个路由是路径参数
+            // if (layer.params) {
+            //     // 如果 /user/lyy/18 匹配 /\/user\/([^\/]*)\/([^\/]*)/
+            //     if (path.test(pathname)) {
+            //         // 匹配到了，说明路径已经匹配到了，取出参数
+            //         let [, ...args] = pathname.match(path)
+            //         // 使用 reduce 赋值给 req.params
+            //         req.params = layer.params.reduce((memo, key, index) => {
+            //             memo[key] = args[index]
+            //             return memo
+            //         }, {})
+            //         return cb(req, res)
+            //     }
+            // }
+            // if ((method === m || m === 'all') && (path === pathname || path === '*')) {
+            //     return cb(req, res)
+            // }
+        // }
         res.end(`Cannot ${method} ${pathname}`)
     }
     // 存放所有请求的信息
     app.routes = []
-    methods = [...methods, 'all']
-    methods.forEach(method => {
+
+    // 中间件先调用，路由在后面
+    app.use = function (path, cb) {
+        if (typeof cb !== 'function') {
+            cb = path
+            path = '/'
+        }
+        let layer = {
+            method: 'middleware',
+            path,
+            cb
+        }
+        app.routes.push(layer)
+    };
+
+    [...methods, 'all'].forEach(method => {
         app[method] = function (path, cb) {
             let layer = {
                 method,
@@ -60,7 +135,49 @@ function Application() {
         let server = http.createServer(app)
         server.listen(...args)
     }
+
+    // 内置的中间件
+    app.use(function (req, res, next) {
+        let { pathname, query } = url.parse(req.url, true)
+        req.path = pathname
+        req.query = query
+        res.sendFile = function (url) {
+            fs.createReadStream(url).pipe(res)
+        }
+        res.send = function (val) {
+            if (typeof val === 'string' || Buffer.isBuffer(val)) {
+                res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+                res.end(val)
+            } else if (typeof val === 'object') {
+                res.setHeader('Content-Type', 'application/json;charset=utf-8')
+                res.end(JSON.stringify(val))
+            } else if (typeof val === Number) {
+                res.statusCode = val
+                res.end(require('_http_server').STATUS_CODES[val])
+            }
+        }
+        next()
+    })
+
     return app
+}
+
+// 返回一个函数
+Application.static = function (pathname) {
+    return function (req, res, next) {
+        let realPath = path.join(pathname, req.path)
+        fs.stat(realPath, function (err, statObj) {
+            if (err) {
+                next()
+            } else {
+                if (statObj.isDirectory()) {
+                    // todo
+                } else {
+                    res.sendFile(realPath)
+                }
+            }
+        })
+    }
 }
 
 module.exports = Application
